09d19bd1d18da82148131729b13995033339e06d
Кристина <dragonfartune@gmail.com>
2023-12-09 18:02:50 +0300

    update static page 2

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -8,48 +8,56 @@ const Statistics: FC = () => {
 
     return (
         <div className={styles.statistics}>
-            <div className={styles.progress}>
-            <div className={styles.progress_item}>
-                <div className={styles.progress1}>
-                <Progress type="dashboard" percent={35} />
+            <div className={styles.left_panel}>
+                <div className={styles.progress}>
+                <div className={styles.progress_item}>
+                    <div className={styles.progress1}>
+                    <Progress type="dashboard" percent={35} />
+                    </div>
+                    <div className={styles.progress_text}> 
+                        Финансовые <br/>нарушения
+                    </div>
                 </div>
-                <div className={styles.progress_text}> 
-                    Финансовые <br/>нарушения
+                <div className={styles.progress_item}>
+                    <div className={styles.progress1}>
+                    <Progress type="dashboard" percent={0} />
+                    </div>
+                    <div className={styles.progress_text}> 
+                        Защита <br/>персональных данных
+                    </div>
+                </div>
+                <div className={styles.progress_item}>
+                    <div className={styles.progress1}>
+                    <Progress type="dashboard" percent={75} />
+                    </div>
+                    <div className={styles.progress_text}> 
+                        Защита личных <br/>цифровых ресурсов
+                    </div>
+                </div>
+                <div className={styles.progress_item}>
+                    <div className={styles.progress1}>
+                    <Progress type="dashboard" percent={100} />
+                    </div>
+                    <div className={styles.progress_text}> 
+                        Правила работы <br/> в сети
+                    </div>
+                </div>
+                </div>
+                <div className={styles.activity}>
+                    <div className={styles.act_text}>
+                    Моя активность
+                    </div>
+                    <div className={styles.main_act}>
+                        <img src="https://i.ibb.co/wSBbmYC/2023-12-09-163211586.png" alt="2023-12-09-163211586"/>
+                    </div>
                 </div>
             </div>
-            <div className={styles.progress_item}>
-                <div className={styles.progress1}>
-                <Progress type="dashboard" percent={0} />
-                </div>
-                <div className={styles.progress_text}> 
-                    Защита <br/>персональных данных
+            <div className={styles.right_panel}>
+                <div className={styles.achieve}>
+                    <img src="https://i.ibb.co/qk6V8ZD/2023-12-09-174241766.png" alt="2023-12-09-174241766"/>
                 </div>
             </div>
-            <div className={styles.progress_item}>
-                <div className={styles.progress1}>
-                <Progress type="dashboard" percent={75} />
-                </div>
-                <div className={styles.progress_text}> 
-                    Защита личных <br/>цифровых ресурсов
-                </div>
-            </div>
-            <div className={styles.progress_item}>
-                <div className={styles.progress1}>
-                <Progress type="dashboard" percent={100} />
-                </div>
-                <div className={styles.progress_text}> 
-                    Правила работы <br/> в сети
-                </div>
-            </div>
-            </div>
-        <div className={styles.activity}>
-            <div className={styles.act_text}>
-            Моя активность
-            </div>
-            <div className={styles.main_act}>
-                <img src="https://i.ibb.co/wSBbmYC/2023-12-09-163211586.png" alt="2023-12-09-163211586"/>
-            </div>
-        </div>
+        
         </div>
         
     )
