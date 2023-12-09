9afcd573e75ede410e7a71bd890fcd1442c2e82a
GAR-07 <garmash.aleksandr.74169701@yandex.ru>
2023-12-09 14:04:45 +0300

    Create Statictics page

---
+++ src/pages/StatisticsPage/StatisticsPage.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+import Statistics from "../../components/Statistics/Statistics";
+
+const StatisticsPage:FC = () => {
+    return (
+        <Statistics/>
+    );
+};
+
+export default StatisticsPage;
