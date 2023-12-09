01b5da806339bdf15a2a76c89f2569b7e28bc5c4
Viacheslav Kolmyk <108751046+Slaik1@users.noreply.github.com>
2023-12-09 21:18:14 +0300

    add tests

--- src/components/Test/Test.tsx
+++ src/components/Test/Test.tsx
@@ -3,8 +3,38 @@ import styles from './Test.module.scss'
 
 const Test = () => {
     return (
-        <div>
+        <div className={styles.lesson}>
+            <div className={styles.title}>Тест 1</div>
+            <div className={styles.contentWrapper}>
+                <div className={styles.content}>
+                    <div className={styles.title}>Основные объекты информационной безопасности:</div>
 
+                    <div className={styles.questionsWrapper}>
+                        <div>
+                            <p>Информационные системы, психологическое состояние пользователей</p>
+                        </div>
+                        <div>
+                            <p>Компьютерные сети, базы данных</p>
+                        </div>
+                    </div>  
+                    <div className={styles.questionsWrapper}>
+                        <div >
+                            <p>Бизнес-ориентированные, коммерческие системы</p>
+                        </div>
+                        <div>
+                            <p>Компьютерные сети, базы данных</p>
+                        </div>
+                    </div>
+                    <div className={styles.buttonWrapper}>
+                        {/* <Button className={styles.btn} type="primary" onClick={() => navigate('/registration')}>
+                            Вернуться
+                        </Button>
+                        <Button className={styles.btn} type="primary" onClick={() => navigate('/registration')}>
+                            Вперёд
+                        </Button> */}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
